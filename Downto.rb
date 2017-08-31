034cdacac115d6675c9364ea6f26b3bbcade11c9
omerkymcu <omerky@gmail.com>
2017-08-31 23:57:20 +0300

    ders1

---
+++ Downto.rb
@@ -0,0 +1,4 @@
+puts"Merhaba benim adım Ruby"
+print"Sana hitap edebilmem için ismini yazarmısın?:"
+isim = gets()
+puts"Memnun oldum #{isim}"
---
+++ ders.rb
@@ -0,0 +1,7 @@
+a = 1
+
+if a == 1
+  puts "a değişkeni 1 sayısına eşittir"
+else
+  puts "a değişkeni 1 sayısına eşit değildir."
+end

93297c698cf8fe9307ed60e834b6863508dc2bc9
omerkymcu <omerky@gmail.com>
2017-09-01 01:39:05 +0300

    ders1

---
+++ ifelse1.rb
@@ -0,0 +1,7 @@
+print"Lütfen Kullanıcı Adı Giriniz : "
+isim = gets.chop
+if isim=="omer"
+  puts"#{isim} Programa Hoş Gelsiniz"
+else
+  puts"#{isim} Yanlış bir kullanıcı adı girdiniz, Program Sonlandırıldı."
+end
